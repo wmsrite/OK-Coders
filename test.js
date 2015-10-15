76f74311445e8f47d2190208edfa0aaaa9d9f654
trekkabout <robertstephens225@hotmail.com>
2015-10-14 22:13:08 -0500

    updates and new content
    …

---
+++ test.js
@@ -0,0 +1,18 @@
+/* comment */
+/* test.js : run this program with `node test.js` */
+
+var name = "philip";
+var age = 32;
+
+console.log("My name is " + name);
+console.log("My age is " + age);
+
+
+var alarm =730;
+var snooze = 9;
+
+var wakeup = alarm + snooze;
+var notice = "You woke up at " + wakeup;
+
+console.log(notice);
+
