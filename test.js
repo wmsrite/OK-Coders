d5e5f830957c910e41f5de6fbbd5318187e2b3e4
trekkabout <robertstephens225@hotmail.com>
2015-11-06 16:07:02 -0600

    new activities
    …

--- test.js
+++ test.js
@@ -1,18 +1,14 @@
 /* comment */
 /* test.js : run this program with `node test.js` */
+/* functions within functions - one function calls the other*/
 
-var name = "philip";
-var age = 32;
+function foo () {
+	console.log("calling foo");
+	bar();
+}
 
-console.log("My name is " + name);
-console.log("My age is " + age);
+function bar () {
+	console.log("calling bar");
+}
 
-
-var alarm =730;
-var snooze = 9;
-
-var wakeup = alarm + snooze;
-var notice = "You woke up at " + wakeup;
-
-console.log(notice);
-
+foo();
---
+++ test1.js
@@ -0,0 +1,19 @@
+/* comment */
+/* test.js : run this program with `node test.js` */
+/* Use functions in other statements, such as if else statementsr*/
+
+var age = 15;
+
+function printOld() {
+    console.log("you're old!");
+}
+
+function printYoung() {
+    console.log("you're young!");
+}
+
+if ( age <= 21 ) {
+    printOld();
+} else {
+    printYoung();
+}
